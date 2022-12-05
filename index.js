7a75463d112410b513073c9f092936f82fd2b2ef
Brian Muller <bamuller@gmail.com>
2022-12-05 18:48:03 -0500

    Update run

--- index.js
+++ index.js
@@ -5,21 +5,25 @@ const fs = require('fs')
 
 const RESULT_PATH = '/tmp/prev-result'
 
-try {
-  let result = core.getInput('result')
-  const sha = github.context.sha
-  core.info('Fetching status from core sha ' + sha)
+const run = async () => {
+  try {
+    let result = core.getInput('result')
+    const sha = github.context.sha
+    core.info('Fetching status from core sha ' + sha)
 
-  const key = 'sha-storage-action-' + sha + '-' + Math.floor(Date.now() / 1000)
-  const cacheKey = await cache.restoreCache([RESULT_PATH], key, ['sha-storage-action-' + sha])
+    const key = 'sha-storage-action-' + sha + '-' + Math.floor(Date.now() / 1000)
+    const cacheKey = await cache.restoreCache([RESULT_PATH], key, ['sha-storage-action-' + sha])
 
-  if (result !== 'unknown') {
-    fs.writeFileSync(RESULT_PATH, result)
-  } else if (fs.existsSync(RESULT_PATH)) {
-    result = fs.readFileSync(RESULT_PATH)
+    if (result !== 'unknown') {
+      fs.writeFileSync(RESULT_PATH, result)
+    } else if (fs.existsSync(RESULT_PATH)) {
+      result = fs.readFileSync(RESULT_PATH)
+    }
+
+    core.setOutput('result', result)
+  } catch(error) {
+    core.setFailed(error.message)
   }
-
-  core.setOutput('result', result)
-} catch(error) {
-  core.setFailed(error.message)
 }
+
+run()
